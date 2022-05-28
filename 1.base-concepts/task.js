fd8ff8b5966a93235483326374e099846f34404c
Utrext <intechros@gmail.com>
2022-05-28 16:36:26 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,27 @@
+"use strict";
+
+// Задача 1
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  } 
+  else if (d === 0) {
+    arr.push(-b / 2 * a);
+    return arr;
+  } 
+  else if (d < 0) {
+  return arr; 
+  }
 }
+
+
+// Задача 2
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
