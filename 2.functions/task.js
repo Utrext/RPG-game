55ee417a14ecae92e3bdfee8f2c3f2c906d39f45
Utrext <intechros@gmail.com>
2022-06-17 19:32:34 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,64 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
 
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0]; 
+  let sum = 0; 
+  let avg = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    
+    sum += arr[i];
+
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
+
+  return {max: max, min: min, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  
+  let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+	  sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  
+  let max = -Infinity;
+  let sum = 0;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+	
+	sum = func(arrOfArr[i]);
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  
+  let param = getArrayParams(arr);
+  let diff = Math.abs(param.max - param.min);
+ 
+  return diff;
 }
