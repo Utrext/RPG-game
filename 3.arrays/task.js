ebdafa61116b60882f52f4e4b5ab449763c2a4de
Utrext <intechros@gmail.com>
2022-06-25 21:04:27 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.every((equal, index) => equal === arr2[index]) && arr1.length === arr2.length; 
+  
+  return result; // true если массивы идентичны
+	 
+};
 
-  // Ваш код
-
-  return result; // boolean
-}
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((num) => num > 0).filter((num) => num % 3 === 0).map((num) => num * 10);
 
-  // Ваш код
-
-  return resultArr; // array
-}
+  return resultArr;
+  
+};
